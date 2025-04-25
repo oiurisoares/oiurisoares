b28697676bfab579466810baf84e391f6420db29
unknown <oiurisoares@gmail.com>
2025-04-25 01:22:39 -0300

    Landing Page: Hero Section

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,45 @@
+import { useRef } from 'react';
+import banner from '../assets/img/banner.png';
+import SocialMediaLinks from '../components/SocialMediaLinks';
+import useScrollReveal from '../hooks/ScrollReveal';
+import './LandingPage.css';
+
+const LandingPage: React.FC = () => {
+    const loadingRef = useRef<HTMLDivElement>(null);
+    useScrollReveal([loadingRef]);
+
+    return (
+        <>
+            <main className='landing-page'>
+                <section className='landing-page__hero no-select'>
+                    <img
+                        alt='background'
+                        className='hero-background blur-load'
+                        loading='lazy'
+                        onLoad={(event) => {
+                            if (event.target instanceof HTMLImageElement) {
+                                event.target.classList.remove('blur');
+                            }
+                        }}
+                        src={banner} />
+
+                    <div className="hero-overlay"></div>
+
+                    <div className='hero-content'>
+                        <h2 className='hero-subtitle'>PORTFOLIO</h2>
+                        <h1 className='hero-title'>Iuri Soares</h1>
+                        <h2 className='hero-description'>FULL-STACK DEVELOPER</h2>
+                    </div>
+
+                    <button className="hero-button" type='button'>CONHECER</button>
+
+                    <footer className="hero-footer" ref={loadingRef}>
+                        <SocialMediaLinks />
+                    </footer>
+                </section>
+            </main>
+        </>
+    );
+}
+
+export default LandingPage;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
 import Loading from "../components/Loading";
+import React from "react";
 
 const LoadingSpinner = () => <Loading />;
+const LandingPage = React.lazy(() => import("../pages/LandingPage"));
+
 const AppRoutes = () => {
     return (
         <Suspense fallback={<LoadingSpinner />}>
             <Routes>
+                <Route path="/" element={< LandingPage />} />
             </Routes>
         </Suspense>
     );
