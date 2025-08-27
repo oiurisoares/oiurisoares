8ffb69ea9e2b66794b660c449d0f0e2689160c1b
Iuri <oiurisoares@gmail.com>
2025-08-27 15:51:06 -0300

    i18n language switcher

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,7 +1,22 @@
 import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
+import FlagBRA from "../assets/img/flag-br.png";
+import FlagUSA from "../assets/img/flag-usa.png";
 import LanguageDetector from "i18next-browser-languagedetector";
+
+export const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem("i18nextLng", lng);
+};
+
+export const getFlagByLanguage = (): string => {
+    const lang = localStorage.getItem("i18nextLng") || "en";
+    switch (lang) {
+        case "pt": return FlagUSA;
+        case "en": default: return FlagBRA;
+    }
+};
 
 i18n.use(Backend)
     .use(initReactI18next)
@@ -20,7 +35,7 @@ i18n.use(Backend)
             }
         },
         debug: true,
-        defaultNS: "layout",
+        defaultNS: "base-layout",
         detection: {
             order: [
                 "navigator", "querystring", "cookie", "localStorage",
@@ -35,11 +50,12 @@ i18n.use(Backend)
             formatSeparator: ",",
         },
         keySeparator: ".",
+        lng: localStorage.getItem("i18nextLng") || "en",
         nonExplicitSupportedLngs: false,
         ns: [
+            "base-layout",
             "footer",
             "landing-page",
-            "layout",
         ],
         react: { useSuspense: true },
         supportedLngs: ["en", "pt",],
